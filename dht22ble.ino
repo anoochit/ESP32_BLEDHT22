b108216024f49aab735fcd6ce8bc2dd628e29357
Anuchit Chalothorn <anoochit@gmail.com>
2020-03-23 17:52:46 +0700

    optimized and reformat code

--- dht22ble.ino
+++ dht22ble.ino
@@ -1,8 +1,3 @@
-/* 
- *  Programa baseado no programa original desenvolvido por Timothy Woo 
- *  Tutorial do projeto original; https://www.hackster.io/botletics/esp32-ble-android-arduino-ide-awesome-81c67d
- *  Modificado para ler dados do sensor DHT11
- */  
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -15,13 +10,14 @@
 #include <iostream>
 #include <string>
 
-BLECharacteristic *pCharacteristic;
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
 
 bool deviceConnected = false;
-const int LED = 2; // Could be different depending on the dev board. I used the DOIT ESP32 dev board.
- 
-#define DHTPIN 23 // pino de dados do DHT11
-#define DHTTYPE DHT22 // DHT 11
+bool oldDeviceConnected = false;
+
+#define DHTPIN 23
+#define DHTTYPE DHT22
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -30,9 +26,8 @@ float temperature;
 
 uint32_t delayMS;
 
-//#define SERVICE_UUID           "0x181A6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define DHTDATA_CHAR_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" 
+//#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -45,94 +40,65 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
-      Serial.println(rxValue[0]);
-
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-
-        for (int i = 0; i < rxValue.length(); i++) {
-          Serial.print(rxValue[i]);
-        }
-        Serial.println();
-        Serial.println("*********");
-      }
-
-      // Processa o caracter recebido do aplicativo. Se for A acende o LED. B apaga o LED
-      if (rxValue.find("A") != -1) { 
-        Serial.println("Turning ON!");
-        digitalWrite(LED, HIGH);
-      }
-      else if (rxValue.find("B") != -1) {
-        Serial.println("Turning OFF!");
-        digitalWrite(LED, LOW);
-      }
-    }
-};
 
 void setup() {
   Serial.begin(115200);
 
-  dht.begin(); 
-  sensor_t sensor; 
+  dht.begin();
+  sensor_t sensor;
   delayMS = sensor.min_delay / 1000;
 
-  pinMode(LED, OUTPUT);
-  
-  BLEDevice::init("BLE-DHT22"); 
-  
-  BLEServer *pServer = BLEDevice::createServer();
+  BLEDevice::init("BLE-DHT22");
+
+  pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
- 
+
   BLEService *pService = pServer->createService((uint16_t)0x181A);
- 
+
   pCharacteristic = pService->createCharacteristic(
-                      DHTDATA_CHAR_UUID,
+                      CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
-                      
-  pCharacteristic->addDescriptor(new BLE2902()); 
- 
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
   pService->start();
- 
+
   pServer->getAdvertising()->start();
   Serial.println("Waiting for a client to connect...");
- }
+}
 
 void loop() {
-  
-  
+
+
   if (deviceConnected) {
     delay(delayMS);
-      
-    
-  // Get temperature event and print its value.
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    temperature=event.temperature;
-//    Serial.print(F("Temperature: "));
-//    Serial.print(event.temperature);
-//    Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    humidity=event.relative_humidity;
-//    Serial.print(F("Humidity: "));
-//    Serial.print(event.relative_humidity);
-//    Serial.println(F("%"));
-  }
-    
+
+    // Get temperature event and print its value.
+    sensors_event_t event;
+
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading data!"));
+    }
+    else {
+      temperature = event.temperature;
+      //    Serial.print(F("Temperature: "));
+      //    Serial.print(event.temperature);
+      //    Serial.println(F("°C"));
+    }
+
+    dht.humidity().getEvent(&event);
+    if ( isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading data!"));
+    }
+    else {
+      humidity = event.relative_humidity;
+      //    Serial.print(F("Humidity: "));
+      //    Serial.print(event.relative_humidity);
+      //    Serial.println(F("%"));
+    }
+
     char humidityString[2];
     char temperatureString[2];
     dtostrf(humidity, 1, 2, humidityString);
@@ -140,13 +106,26 @@ void loop() {
 
     char dhtDataString[16];
     sprintf(dhtDataString, "%.02f,%.02f", temperature, humidity);
-    
+
     pCharacteristic->setValue(dhtDataString);
-    
-    pCharacteristic->notify(); 
+
+    pCharacteristic->notify();
     Serial.print("*** Data String: ");
     Serial.print(dhtDataString);
     Serial.println(" ***");
   }
-  
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+
 }
